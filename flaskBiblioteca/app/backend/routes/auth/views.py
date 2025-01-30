c4f19ee9001fa2979c2958e20baf7420d6c3573a
Milton-Luis <miltonluis138@gmail.com>
2025-01-30 16:24:44 -0300

    `Removed unused imports and Services calls from auth views`

--- flaskBiblioteca/app/backend/routes/auth/views.py
+++ flaskBiblioteca/app/backend/routes/auth/views.py
@@ -6,7 +6,6 @@ from flask_login import current_user, login_required, login_user, logout_user
 from app.backend.extensions.database import db
 from app.backend.extensions.security import check_password, validate_token
 from app.backend.model.models import User
-from app.backend.utils.operations import Services
 
 from . import auth
 from .forms import LoginForm
@@ -22,7 +21,6 @@ def login():
     if form.validate_on_submit():
         user = db.session.query(User).filter_by(email=form.email.data).first()
 
-        # user = Services.get_first_record(User, email=form.email.data)
 
         if user and check_password(user.roles.password, form.password.data):
             login_user(user)
@@ -51,7 +49,6 @@ def logout():
 def confirm_email(token):
     email = validate_token(token)
     user = User.query.filter_by(email=email).first_or_404()
-    user = Services.get_first_record(User, email=email)
 
     if user.roles.is_confirmed:
         return redirect(url_for("main.index"))
