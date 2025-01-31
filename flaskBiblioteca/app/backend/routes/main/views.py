c4a16a15845995924754b885631498a03128f64a
Milton-Luis <miltonluis138@gmail.com>
2025-01-31 17:50:54 -0300

    `Refactored views in main module to improve code organization and consistency.`

--- flaskBiblioteca/app/backend/routes/main/views.py
+++ flaskBiblioteca/app/backend/routes/main/views.py
@@ -1,17 +1,21 @@
+from app.backend.extensions.database import db
+from app.backend.model.models import Books, LendingBooks
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
-from app.backend.model.models import Books, LendingBooks, User
-
 from . import main
-from .forms import LendingForm, NewBookForm, SearchBookForm
-from app.backend.extensions.database import db
+from .forms import BookForm, LendingForm, SearchBookForm
+
 
 @main.route("/")
 def index():
     if current_user.is_anonymous:
         return redirect(url_for("auth.login"))
-    return render_template("pages/index.html", title="Início")
+
+    book = Books()
+
+    context = {"book": book, "title": "Início"}
+    return render_template("pages/index.html", **context)
 
 
 @main.route("/livros/", methods=["POST", "GET"])
@@ -21,61 +25,62 @@ def books():
 
     page = request.args.get("page", 1, type=int)
 
-    books = db.session.query(Books).filter(Books.title).paginate(
-        page=page, per_page=10, error_out=True
-    )
-    return render_template(
-        "pages/books.html",
-        books=books,
-        endpoint="main.books",
-        form=form,
-        title="Livros",
-    )
+    books = db.session.query(Books).paginate(page=page, per_page=10, error_out=True)
+
+    context = {
+        "books": books,
+        "endpoint": "main.books",
+        "form": form,
+        "title": "Livros",
+    }
+    return render_template("pages/books.html", **context)
 
 
-@main.route("/livros/<int:book_id>/", methods=["GET", "POST"])
+@main.route("/livros/detalhes/<int:book_id>/", methods=["GET", "POST"])
 @login_required
-def showBook(book_id):
-    book = db.session.query(Books).get(book_id)
-    return render_template(
-        "pages/showbook.html",
-        book=book,
-        title=f"Livro - {book.title}",
-    )
+def book_details(book_id):
+    book = db.session.get(Books, book_id)
+
+    context = {"book": book, "title": f"Livro - {book.title}"}
+    return render_template("pages/book_detail.html", **context)
 
 
 @main.route("/emprestimos/")
 @login_required
-def loanBooks():
-    loans = db.session.query(Books).filter(Books.title).paginate(
-        page=1, per_page=5, error_out=True
+def lendings():
+    loans = (
+        db.session.query(Books)
+        .filter(Books.title)
+        .paginate(page=1, per_page=5, error_out=True)
     )
 
-    return render_template(
-        "pages/loanBooks.html",
-        loans=loans,
-        endpoint="main.loanBooks",
-        title="Empréstimos",
-    )
+    context = {
+        "loans": loans,
+        "endpoint": "main.lendings",
+        "title": "Empréstimos",
+    }
+    return render_template("pages/lendings.html", **context)
 
 
 @main.route("/livros/novo/", methods=["GET", "POST"])
 @login_required
 def new_book():
-    form = NewBookForm()
+    form = BookForm()
     if form.validate_on_submit():
         books = Books(
             title=form.title.data,
             author=form.author.data,
             quantity_of_books=form.quantity.data,
-            isbn=form.isbnCode.data,
+            isbn=form.isbn.data,
         )
         db.session.add(books)
         db.session.commit()
 
         flash("Livro adicionado com sucesso!", "success")
         return redirect(url_for("main.index"))
-    return render_template("pages/newBook.html", form=form, title="Novo Livro")
+
+    context = {"form": form, "title": "Novo Livro"}
+    return render_template("pages/new_book.html", **context)
 
 
 @main.route("/emprestimos/novo/", methods=["GET", "POST"])
@@ -91,11 +96,15 @@ def new_loan():
             quantityBooks=form.quantity.data,
             loan_date=form.loan_date.data,
             return_date=form.return_date.data,
+            users=User()
         )
         lending.save()
 
         flash("Empréstimo realizado!", "success")
         return redirect(url_for("main.index"))
-    return render_template(
-        "pages/newLoan.html", form=form, title="Novo empréstimo"
-    )
+
+    context = {
+        "form": form,
+        "title": "Novo Empréstimo",
+    }
+    return render_template("pages/new_lending.html", **context)
