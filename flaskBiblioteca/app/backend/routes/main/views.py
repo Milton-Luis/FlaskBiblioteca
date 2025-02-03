e912e6afdd1a9ad56c3e0b79f1e8d44d9dd5a349
Milton-Luis <miltonluis138@gmail.com>
2025-02-03 12:16:15 -0300

    `Added Students and User models to views.py, modified database queries and added new variables to templates.`

--- flaskBiblioteca/app/backend/routes/main/views.py
+++ flaskBiblioteca/app/backend/routes/main/views.py
@@ -1,7 +1,8 @@
 from app.backend.extensions.database import db
-from app.backend.model.models import Books, LendingBooks
+from app.backend.model.models import Books, LendingBooks, Students, User
 from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
+from sqlalchemy.sql import asc
 
 from . import main
 from .forms import BookForm, LendingForm, SearchBookForm
@@ -24,8 +25,11 @@ def books():
     form = SearchBookForm()
 
     page = request.args.get("page", 1, type=int)
-
-    books = db.session.query(Books).paginate(page=page, per_page=10, error_out=True)
+    books = (
+        db.session.query(Books)
+        .order_by(asc(Books.title))
+        .paginate(page=page, per_page=3, error_out=True)
+    )
 
     context = {
         "books": books,
@@ -51,6 +55,7 @@ def lendings():
     loans = (
         db.session.query(Books)
         .filter(Books.title)
+        .order_by(asc(Books.title))
         .paginate(page=1, per_page=5, error_out=True)
     )
 
@@ -83,22 +88,27 @@ def new_book():
     return render_template("pages/new_book.html", **context)
 
 
-@main.route("/emprestimos/novo/", methods=["GET", "POST"])
+@main.route("/emprestimos/novo/<int:id>", methods=["GET", "POST"])
 @login_required
-def new_loan():
+def new_loan(id):
     form = LendingForm()
+
+    get_user = db.session.query(User).all()
+
+    get_book = db.session.query(Books).filter(Books.id == id).one()
     if form.validate_on_submit():
-        # get_book = Services.get_one_record(Book, Book.id)
+        # stmt = db.select(User).join(Students).where(User.id == Students.user_id)
+        # make_lending = db.session.execute(stmt).scalars().all()
 
         # get_user = Services.get_first_record(User, User.firstname)
 
-        lending = LendingForm(
-            quantityBooks=form.quantity.data,
-            loan_date=form.loan_date.data,
-            return_date=form.return_date.data,
-            users=User()
-        )
-        lending.save()
+        # lending = LendingForm(
+        #     quantityBooks=form.quantity.data,
+        #     loan_date=form.loan_date.data,
+        #     return_date=form.return_date.data,
+        #     users=User(User.set_fullname(form.fullname.data))
+        # )
+        # lending.save()
 
         flash("Empréstimo realizado!", "success")
         return redirect(url_for("main.index"))
@@ -106,5 +116,7 @@ def new_loan():
     context = {
         "form": form,
         "title": "Novo Empréstimo",
+        "get_user": get_user,
+        "get_book": get_book,
     }
     return render_template("pages/new_lending.html", **context)
