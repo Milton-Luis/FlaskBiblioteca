349aaaf362bf5c8fae848c935f876b89229aab3c
Milton-Luis <miltonluis138@gmail.com>
2025-02-12 12:57:19 -0300

    refactor: Refactored forms in main routes: removed unused imports, updated field render keywords and added new fields

--- flaskBiblioteca/app/backend/routes/main/forms.py
+++ flaskBiblioteca/app/backend/routes/main/forms.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask_wtf import FlaskForm
 from wtforms.fields import (
     DateField,
@@ -7,29 +5,22 @@ from wtforms.fields import (
     SelectField,
     StringField,
     SubmitField,
+    SearchField
 )
 from wtforms.validators import DataRequired, NumberRange
 
 
 class BookForm(FlaskForm):
-    # search = StringField(
-    #     None,
-    #     validators=[DataRequired()],
-    #     render_kw={
-    #         "placeholder": "título do livro ou nome do autor",
-    #         "class": "form-control",
-    #     },
-    #     id="searchBook",
-    # )
+    
     title = StringField(
         "Título",
         validators=[DataRequired()],
-        render_kw={"placeholder": "título do livro"},
+        render_kw={"placeholder": "Título do livro"},
     )
     author = StringField(
         "Autor",
         validators=[DataRequired()],
-        render_kw={"placeholder": "autor do livro"},
+        render_kw={"placeholder": "Autor do livro"},
     )
     isbn = StringField(
         "ISBN",
@@ -53,33 +44,30 @@ class BookForm(FlaskForm):
     submit = SubmitField("Adicionar novo livro")
 
 
-class LendingForm(FlaskForm):
-    person = StringField(
-        "Nome", validators=[DataRequired()], render_kw={"placeholder": "Nome"}
-    )
+class LendingBooksForm(FlaskForm):
+    search = StringField(render_kw={"placeholder": "Digite o nome do locatario"})
     title = StringField(
         "Título do livro",
         validators=[DataRequired()],
+        render_kw={"disabled": "disabled"},
+    )
+    quantity = IntegerField(
+        "Quantidade a ser alugada",
+        validators=[DataRequired(), NumberRange(min=1, max=50)],
+        render_kw={"value": 1},
     )
 
-    loan_date = DateField("Data do empréstimo", render_kw={"disabled", "disabled"})
-    return_date = DateField("Data do empréstimo")
-    quantity = IntegerField(
-        "Quantidade de livros", validators=[DataRequired(), NumberRange(min=1, max=50)]
+    lending_date = DateField(
+        "Data do empréstimo",
+        render_kw={"disabled": "disabled"},
+    )
+    return_date = DateField(
+        "Data de devolução",
+        validators=[DataRequired()],
     )
     submit = SubmitField("Efetuar empréstimo")
 
 
 class SearchBookForm(FlaskForm):
-    search = StringField(
-        None,
-        validators=[DataRequired()],
-        render_kw={
-            "placeholder": "título do livro ou nome do autor",
-            "class": "form-control",
-        },
-        id="searchBook",
-    )
-    submit = SubmitField(
-        render_kw={"value": "", "class": "btn submit-btn"},
-    )
+    search = SearchField()
+    
