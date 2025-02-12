5ec9141bfff2c4989de7ea31ae2beedd9d1b89d5
Milton-Luis <miltonluis138@gmail.com>
2025-02-12 13:03:45 -0300

    refactor/feat: Refactored scripts.js: moved lock function to event listener, added date formatting and zero padding functions, and updated submit button event listener.

--- flaskBiblioteca/app/frontend/static/js/scripts.js
+++ flaskBiblioteca/app/frontend/static/js/scripts.js
@@ -1,33 +1,43 @@
 // window.location.reload();
-const lock = () => {
-	let password = document.querySelector("input[type='password']");
-	let lockIcon = document.getElementById("lockUnlock");
+document.addEventListener("DOMContentLoaded", function () {
+	const lock = () => {
 
-	password.addEventListener("input", () => {
-		if (password.value.length > 0) {
-			lockIcon.classList.remove("fa-unlock");
-			lockIcon.classList.add("fa-lock");
-		} else {
-			lockIcon.classList.remove("fa-lock");
-			lockIcon.classList.add("fa-unlock");
-		}
-	});
-};
-lock();
-
-const submitBtn = document.querySelector(".submit-login");
-
-
-const checkNullForm = () => {
-	const inputs = document.querySelector(".checkInput");
-	if (!inputs.value) return;
-};
-
-submitBtn.addEventListener("click", () => {
-	checkNullForm();
+		const password = document.querySelector("input[type='password']");
+		const lockIcon = document.getElementById("lockUnlock");
+		
+		password.addEventListener("input", () => {
+			if (password.value.length > 0) {
+				lockIcon.classList.remove("fa-unlock");
+				lockIcon.classList.add("fa-lock");
+			} else {
+				lockIcon.classList.remove("fa-lock");
+				lockIcon.classList.add("fa-unlock");
+			}
+		});
+	};
 });
+document.addEventListener("DOMContentLoaded", function () {
+	
+	const add_zero = (num) => {
+		return num >= 10 ? num : `0${num}`;
+	};
 
-// const displayCurrentDate =() =>{
-// 	const currentDate = Date.now();
-// 	const currentMonth
-// }
+	const format_date = (date) => {
+		const day = date.getDate().toString().padStart(2, "0");
+		const month = (date.getMonth() + 1).toString().padStart(2, "0");
+		const year = date.getFullYear();
+		
+		return `${year}-${month}-${day}`;
+		/**
+		 * por padrão deve seguir essa configuração de retorno quando exibido num form, o navegador
+		 * irá alterar o formato de data conforme a localidade em que esitver. 
+		 * Ex:Brasil - dd/mm/yyyy
+		 *    Canada - mm/dd/yyyy
+		 */
+	};
+
+	const date = new Date();
+	const formated_date = format_date(date);
+	
+	document.querySelector("#lending_date").value = formated_date;
+});
