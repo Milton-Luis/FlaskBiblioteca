0e0969800d6aefda326a9622d74777c22da0723d
Milton-Luis <miltonluis138@gmail.com>
2025-03-03 17:11:17 -0300

    Refactor: implement dynamic search functionality for book titles with improved user feedback

--- flaskBiblioteca/app/frontend/static/js/scripts.js
+++ flaskBiblioteca/app/frontend/static/js/scripts.js
@@ -8,10 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
 				message.removeChild(div);
 			}, 2000);
 		}
-	}
+	};
 	removeMessage();
 });
-
 
 document.addEventListener("DOMContentLoaded", function () {
 	const login = () => {
@@ -33,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
 					div.textContent = "Preencha todos os campos!";
 					message.appendChild(div);
 				}
-
 			}
 		});
 
@@ -58,9 +56,9 @@ document.addEventListener("DOMContentLoaded", function () {
 		const day = date.getDate().toString().padStart(2, "0");
 		const month = (date.getMonth() + 1).toString().padStart(2, "0");
 		const year = date.getFullYear();
-		const hour = (date.getHours().toString().padStart(2, "0"));
-		const minute = (date.getMinutes().toString().padStart(2, "0"));
-		const second = (date.getSeconds().toString().padStart(2, "0"));
+		const hour = date.getHours().toString().padStart(2, "0");
+		const minute = date.getMinutes().toString().padStart(2, "0");
+		const second = date.getSeconds().toString().padStart(2, "0");
 
 		return `${year}-${month}-${day}`;
 		/**
@@ -103,3 +101,71 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+	const searchInput = document.getElementById("search");
+	const searchList = document.querySelector("#search-list");
+
+	const urlParts = window.location.pathname.split("/");
+	const bookTitle = urlParts[urlParts.length - 2];
+
+	let selectedName = "";
+
+	searchInput.addEventListener("input", function () {
+		const query = searchInput.value.trim();
+
+		if (query === "") {
+			searchList.innerHTML = "";
+			return;
+		}
+
+		fetch(`/emprestimos/novo/${bookTitle}/search?q=${query}`)
+			.then((response) => response.json())
+			.then((data) => {
+				searchList.innerHTML = "";
+
+				if (data.length === 0) {
+					const noResults = document.createElement("li");
+					noResults.classList.add("mt-2", "pl-2")
+					noResults.textContent = "Nome não encontrado!";
+					searchList.appendChild(noResults);
+				} else {
+					data.forEach((name) => {
+						const li = document.createElement("li");
+						li.classList.add(
+							"cursor-pointer",
+							"hover:bg-slate-500",
+							"hover:text-white",
+							"py-2",
+							"pl-2",
+							"border",
+							"border-slate-400",
+							"w-full",
+							"flex",
+							"justify-start",
+							"gap-8",
+							"first:mt-2"
+							
+						);
+						const nameParagraph = document.createElement("p");
+						const infoParagraph = document.createElement("p");
+
+						const infoText = name.info.length > 0 ? name.info[0].info : "Sem informação";
+
+						nameParagraph.textContent = `Nome: ${name.fullname}`;
+						infoParagraph.textContent = `${infoText}`;
+
+						li.addEventListener("click", () => {
+							selectedName = name.fullname;
+							setTimeout(() => {
+								searchInput.value = selectedName;
+							}, 10);
+							searchList.innerHTML = "";
+						});
+						li.appendChild(nameParagraph);
+						li.appendChild(infoParagraph);
+						searchList.appendChild(li);
+					});
+				}
+			});
+	});
+});
