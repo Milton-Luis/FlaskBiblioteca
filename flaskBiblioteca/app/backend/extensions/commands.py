de26eb2d1fe0c9d98e8588e4bcf6edd84c45eb51
Milton-Luis <miltonluis138@gmail.com>
2025-03-03 17:09:31 -0300

    Refactor: improve code readability and structure in commands.py by organizing imports and enhancing user creation logic

--- flaskBiblioteca/app/backend/extensions/commands.py
+++ flaskBiblioteca/app/backend/extensions/commands.py
@@ -1,9 +1,17 @@
 from getpass import getpass
+import os
 
 from app.backend.extensions.database import db
 from app.backend.extensions.security import generate_password
-from app.backend.model.models import (Admin, Books, LendingBooks, Librarian,
-                                      Role, Students, User)
+from app.backend.model.models import (
+    Admin,
+    Books,
+    LendingBooks,
+    Librarian,
+    Role,
+    Students,
+    User,
+)
 
 
 def create_db():
@@ -17,19 +25,33 @@ def drop_db():
     db.drop_all()
     print("Cleaned!")
 
+
 def createSuperUser():
     """Create a super user"""
+    os.system("clear")
     print("Bem vindo ao shell para criação de acesso admin")
-    firstname = input("informe seu nome: ").capitalize()
-    lastname = input("informe seu sobrenome: ").capitalize()
+    firstname = input("Informe seu nome: ").capitalize()
+    lastname = input("Informe seu sobrenome: ").capitalize()
     email = input("Informe seu email: ")
     password = getpass("Informe sua senha: ")
-    role="admin"
-    admin = User(firstname=firstname, lastname=lastname, email=email, roles=Admin(rolename=role, is_admin=True, is_confirmed=True,  password=generate_password(password)))
+    role = "admin"
+    admin = User(
+        firstname=firstname,
+        lastname=lastname,
+        email=email,
+        roles=Admin(
+            rolename=role,
+            is_admin=True,
+            is_confirmed=True,
+            password=generate_password(password),
+        ),
+    )
+    admin.fullname = admin.set_fullname(firstname, lastname)
 
     db.session.add(admin)
     db.session.commit()
     print("Super user created!")
+
 
 def init_app(app):
     for command in [create_db, drop_db, createSuperUser]:
@@ -45,5 +67,5 @@ def init_app(app):
             "admin": Admin,
             "librarian": Librarian,
             "books": Books,
-            "lending_books": LendingBooks
+            "lending_books": LendingBooks,
         }
