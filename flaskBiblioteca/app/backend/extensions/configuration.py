3213c114e1d26d8eb7da02e141aad75351b37474
Milton-Luis <miltonluis138@gmail.com>
2025-04-23 15:36:03 -0300

    Refactor: rename and enhance load_extensions function for better clarity and error handling

--- flaskBiblioteca/app/backend/extensions/configuration.py
+++ flaskBiblioteca/app/backend/extensions/configuration.py
@@ -1,10 +1,26 @@
+from importlib import import_module
+
 from dynaconf import FlaskDynaconf
 
 
-def loader_extension(app):
-    """Load all the extensions"""
-    return app.config.load_extensions(key="EXTENSIONS")
+def load_extensions(app):
+    extensions = app.config.get("EXTENSIONS", [])
 
+    if not extensions:
+        app.logger.warning("No extensions found in the configuration.")
+        return
 
-def init_app(app):
-    return FlaskDynaconf(app)
+    for extension in extensions:
+        try:
+            if ":" not in extension:
+                app.logger.error(f"Invalid format for extension: {extension}. Expected 'module:factory'.")
+            else:
+                module_name, factory = extension.split(":")
+                ext = import_module(module_name)
+                extension_class = getattr(ext, factory)
+                extension_class(app)
+        except Exception as e:
+            app.logger.error(f"Failed to load extension: {extension}. Error: {e}")
+
+def init_app(app, **config):
+    return FlaskDynaconf(app, **config)
