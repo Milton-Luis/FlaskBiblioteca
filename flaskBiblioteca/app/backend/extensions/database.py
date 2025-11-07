d3c9be1ecc618a2f61d762e08fda5af71e0ee9f5
Milton Luís <miltonluis138@gmail.com>
2025-11-07 18:53:44 -0300

    Refactor:  comments removed

--- flaskBiblioteca/app/backend/extensions/database.py
+++ flaskBiblioteca/app/backend/extensions/database.py
@@ -2,22 +2,6 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
-# from sqlalchemy.orm import registry
-
-# def mapper_config():
-#     """mapper_config
-
-#     this function is similiar to the Base superclass with declarativeBase
-#     * variables -> [mapper_registry]
-
-#     * [registry()] function that creates new 'MapperRegistry, used to manage mappings between classes and database tables
-
-#     Returns:
-#         mapper_registry: returns the generate_base() function generates a new base class that contain metada and methods for all mapped classes
-#     """
-#     mapper_registry = registry()
-#     return mapper_registry.generate_base()
-
 
 class Base(DeclarativeBase):
     pass
