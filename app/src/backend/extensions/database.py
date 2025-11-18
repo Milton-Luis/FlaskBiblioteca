93c8ec4ef1c757f41b5f12a474f36cf4281188d1
Milton Luís <miltonluis138@gmail.com>
2025-11-18 10:52:42 -0300

    Feat: implement database extension with SQLAlchemy and Flask-Migrate

---
+++ app/src/backend/extensions/database.py
@@ -0,0 +1,20 @@
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base, session_options={"expire_on_commit": False})
+
+migrate = Migrate()
+
+
+
+def init_app(app):
+   
+    db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
