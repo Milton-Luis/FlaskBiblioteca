cae737062452fa7e0381259f9c9b9576ad9d9cf7
Milton Luís <miltonluis138@gmail.com>
2026-04-17 22:12:09 -0300

    Test: Add test_instance_path to verify database URI configuration

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -1,1 +1,9 @@
-assert app.instance_path in app.config["SQLALCHEMY_DATABASE_URI"]
+from pathlib import Path
+
+def test_instance_path(app):
+    db_path = Path(app.config["SQLALCHEMY_DATABASE_URI"].replace("sqlite:///", ""))
+    print(db_path)
+    assert db_path.parent == Path(app.instance_path)
+
+# def app_name():
+#     assert ...
