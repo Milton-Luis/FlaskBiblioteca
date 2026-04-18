81fdfc7013cc02a43bbc3b00f2221d4c430ae3f8
Milton Luís <miltonluis138@gmail.com>
2026-04-17 22:07:28 -0300

    Feat: Book models created from models file, and add methods for managing book quantities and calculating total books

--- app/src/backend/models/books.py
+++ app/src/backend/models/books.py
@@ -1,11 +1,13 @@
 from typing import TYPE_CHECKING
 
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.backend.extensions.database import db
 
 if TYPE_CHECKING:
     from .lending import LendingBooks
+
 
 class Books(db.Model):
     __tablename__ = "books"
@@ -19,6 +21,24 @@ class Books(db.Model):
     available_quantity: Mapped[int] = mapped_column(nullable=False, default=0)
 
     users: Mapped[list["LendingBooks"]] = db.relationship(back_populates="books")
+
+    def add_available_quantity(book_id: int, quantity_to_lend: int) -> int:
+        book = db.session.query(Books).filter_by(id=book_id).first()
+        if quantity_to_lend > book.available_quantity:
+            raise ValueError("mais livros disponíveis do que o total")
+        book.available_quantity += quantity_to_lend
+        return book.available_quantity
+
+    def subtract_available_quantity(book_id: int, quantity_to_lend: int) -> int:
+        book = db.session.query(Books).filter_by(id=book_id).first()
+        if quantity_to_lend > book.available_quantity:
+            raise ValueError("Quantidade de livros indisponível")
+        book.available_quantity -= quantity_to_lend
+        return book.available_quantity
+
+    def sum_total_of_books():
+        total_books = db.session.query(func.sum(Books.total_of_books)).scalar()
+        return total_books or 0
 
     def __repr__(self) -> str:
         return f"Livro(s): {self.title} - Autor: {self.author} - Quantidade: {self.total_of_books} - Disponível: {self.available_quantity}"
