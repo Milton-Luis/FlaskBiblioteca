f7b5797d9b58b54a2bb51d6b10217a98a4030f88
Milton Luís <miltonluis138@gmail.com>
2026-04-17 22:09:13 -0300

    Feat: Lending Book model created from models file and add methods to display date formart and manage lendings dates and confirmations

--- app/src/backend/models/lending.py
+++ app/src/backend/models/lending.py
@@ -23,4 +23,36 @@ class LendingBooks(db.Model):
 
     lending_date: Mapped[datetime] = mapped_column(nullable=False)
     return_date: Mapped[datetime | None] = mapped_column(nullable=True)
-    quantity_lent: Mapped[int] = mapped_column(nullable=False, default=1)
+
+    quantity_lent: Mapped[int] = mapped_column(nullable=False)
+
+    def get_formated_date(self, date: datetime) -> str:
+        return date.strftime("%d/%m/%Y")
+
+    def count_delayed_books(self) -> int:
+        current_date = datetime.now().date()
+        delayed_books_count = 0
+        lending_records = LendingBooks.query.all()
+        for lending_record in lending_records:
+            if lending_record.return_date.date() < current_date:
+                delayed_books_count += 1
+        return delayed_books_count
+
+    def count_books_due_today(self) -> int:
+        today = datetime.now().date()
+        books_due_today_count = 0
+
+        lending_records = LendingBooks.query.all()
+        for lending_record in lending_records:
+            if lending_record.return_date.date() == today:
+                books_due_today_count += 1
+        return books_due_today_count
+
+    def has_active_loan(self,user_id, book_id):
+        return db.session.query(
+            db.exists().where(
+                LendingBooks.book_id == book_id,
+                LendingBooks.user_id == user_id,
+                LendingBooks.return_date.is_(None),
+            )
+        ).scalar()
