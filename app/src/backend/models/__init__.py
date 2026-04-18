3e26b0bf88f4f5499b48707159197b394188911a
Milton Luís <miltonluis138@gmail.com>
2026-04-17 22:05:53 -0300

    Feat: Add Roles model to the __all__ export in __init__.py

--- app/src/backend/models/__init__.py
+++ app/src/backend/models/__init__.py
@@ -1,5 +1,6 @@
 from .books import Books
 from .lending import LendingBooks
+from .roles import Roles
 from .users import User
 
-__all__ = ["User", "Books", "LendingBooks"]
+__all__ = ["User", "Books", "LendingBooks", "Roles"]
