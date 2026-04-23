15faca7d8fe2ac1012f2fa8a5475f4cbd9da65e5
Milton Luís <miltonluis138@gmail.com>
2026-04-23 16:13:49 -0300

    refactor: clean up LoginForm and remove unused AddLibrarianForm

--- app/src/backend/routes/auth/forms.py
+++ app/src/backend/routes/auth/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo, Length
-
-from src.backend.models.users import User
+from wtforms import EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(FlaskForm):
     email = EmailField(
         "Email",
-        validators=[DataRequired(message="Email é obrigatório")],
+        validators=[
+            DataRequired(message="Email é obrigatório"),
+            Email(message="Email invalido"),
+        ],
         render_kw={"placeholder": "Digite seu email"},
     )
     password = PasswordField(
@@ -18,43 +19,3 @@ class LoginForm(FlaskForm):
     )
     submit = SubmitField("Entrar")
 
-
-class AddLibrarianForm(FlaskForm):
-    firstname = StringField(
-        "Nome",
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Digite seu nome"},
-    )
-    lastname = StringField(
-        "Sobrenome",
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Digite seu sobrenome"},
-    )
-    email = EmailField(
-        "E-mail",
-        validators=[DataRequired(), Length(min=1, max=65), Email()],
-        render_kw={"placeholder": "Digite seu email"},
-    )
-    phone = StringField(
-        "Telefone",
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Digite seu telefone"},
-    )
-    password = PasswordField(
-        "Senha",
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Digite sua senha"},
-    )
-    confirm_password = PasswordField(
-        "Confirmar senha",
-        validators=[
-            DataRequired(),
-            EqualTo("password", "As senhas devem conrresponder"),
-        ],
-        render_kw={"placeholder": "Confirme sua senha"},
-    )
-    signup = SubmitField("Inserir Registro")
-
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError("Email already registered.")
