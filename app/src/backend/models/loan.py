d244dea2ca756b8ec8ab10c572d9129b990190b8
Milton Luís <miltonluis138@gmail.com>
2026-06-13 15:11:23 -0300

    feat: implement status property for BookLoan model and update overdue checks

--- app/src/backend/models/loan.py
+++ app/src/backend/models/loan.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from typing import TYPE_CHECKING
 
+from sqlalchemy import and_, func
 from sqlalchemy.orm import Mapped, mapped_column
-from src.backend.utils.utils import format_date
+from sqlalchemy.ext.hybrid import hybrid_property
+from src.backend.utils.utils import format_date, today_date
 from src.backend.extensions.database import db
+from src.backend.components.components import STATUS_MAPPING
 
 if TYPE_CHECKING:
     from .books import Books
@@ -25,18 +28,50 @@ class BookLoan(db.Model):
     due_date: Mapped[datetime] = mapped_column(nullable=False)
     return_date: Mapped[datetime | None] = mapped_column(nullable=True)
 
+    @hybrid_property
+    def status(self) -> str:
+        today = today_date()
+
+        if self.return_date is not None:
+            return STATUS_MAPPING["returned"]
+        elif self.due_date.date() < today:
+            return STATUS_MAPPING["overdue"]
+        elif self.due_date.date() == today:
+            return STATUS_MAPPING["due_today"]
+        else:
+            return STATUS_MAPPING["on_time"]
+
+    @status.expression
+    def status(cls):
+        today = func.current_date()
+
+        return db.case(
+            (cls.return_date.is_not(None), STATUS_MAPPING["returned"]),  # noqa E711
+            (
+                and_(cls.return_date.is_(None), func.date(cls.due_date) == today),
+                STATUS_MAPPING["due_today"],
+            ),
+            (
+                and_(cls.return_date.is_(None), func.date(cls.due_date) < today),
+                STATUS_MAPPING["overdue"],
+            ),
+            else_=STATUS_MAPPING["on_time"],
+        )
+
     def is_overdue(self) -> bool:
         """Verifica se o livro está atrasado com base na data de retorno."""
-        return self.return_date and self.return_date.date() < datetime.now().date()
+        today = today_date()
+        return (self.return_date is None) and (self.due_date.date() < today)
 
     def is_due_today(self) -> bool:
         """Verifica se o livro tem data de retorno para hoje."""
-        return self.return_date and self.return_date.date() == datetime.now().date()
+        today = today_date()
+        return (self.return_date is None) and (self.due_date.date() == today)
 
     def formatted_loan_date(self) -> str:
         """Formata a data de empréstimo para exibição."""
         return format_date(self.loan_date)
-    
+
     def formatted_due_date(self) -> str:
         """Formata a data de retorno para exibição."""
-        return format_date(self.due_date)   
+        return format_date(self.due_date)
