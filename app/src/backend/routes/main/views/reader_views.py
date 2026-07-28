dcbfaa42287b5c39d78ca9e9e296e67c8642aa90
Milton Luís <miltonluis138@gmail.com>
2026-07-28 16:10:06 -0300

    fix: update route and flash messages for new reader functionality

--- app/src/backend/routes/main/views/reader_views.py
+++ app/src/backend/routes/main/views/reader_views.py
@@ -6,7 +6,7 @@ from src.backend.routes.main.forms import ReaderForm
 from src.backend.services import reader_service
 
 
-@main.route("/leitor/novo", methods=["POST", "GET"])
+@main.route("/reader/novo", methods=["POST", "GET"])
 @login_required
 def new_reader():
     form = ReaderForm()
@@ -16,12 +16,12 @@ def new_reader():
             reader_service.create_reader(form)
             db.session.commit()
 
-            flash("Novo leitor adicionado!!", "success")
+            flash("Leitor adicionado!!", "success")
         except Exception:
+            flash("Erro ao adicionar leitor", "danger")
             db.session.rollback()
-            flash("Erro ao adicionar informações", "danger")
         return redirect(url_for("main.index"))
 
     return render_template(
-        "pages/new_reader.html", form=form, title="Adicionar novo Leitor"
+        "pages/new_reader.html", form=form, title="Adicionar leitor"
     )
