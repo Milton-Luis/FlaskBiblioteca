858c7d24545d29934941a0f9dec37352e77e611c
Milton Luís <miltonluis138@gmail.com>
2026-08-12 15:56:35 -0300

    refactor: upadte loan handling to use SearchForm and improve reader validation

--- app/src/backend/routes/main/views/loan_views.py
+++ app/src/backend/routes/main/views/loan_views.py
@@ -5,8 +5,8 @@ from src.backend.models.books import Books
 from src.backend.models.loan import BookLoan
 from src.backend.models.reader import Reader
 from src.backend.routes.main import main
-from src.backend.routes.main.forms import BookLoanForm, SearchBookForm
-from src.backend.services import book_service, loan_service
+from src.backend.routes.main.forms import BookLoanForm, SearchForm
+from src.backend.services import book_service, loan_service, reader_service
 
 
 @main.route("/emprestimos")
@@ -25,55 +25,35 @@ def loans_page():
 @main.route("/emprestimos/novo/<slug>", methods=["POST", "GET"])
 @login_required
 def new_loan(slug):
-    session_id = session.get("reader_id")
+    loan_form = BookLoanForm()
+    search_form = SearchForm()
 
-    form = BookLoanForm()
-    search_form = SearchBookForm()
+    reader_id = session.get("reader_id")
+    reader = reader_service.get_valid_reader(reader_id)
+    book = book_service.get_book_by_slug(slug)
 
-    reader = db.session.query(Reader).filter_by(id=session_id).first()
-    book = db.session.query(Books).filter_by(slug=slug).first()
+    if book:
+        loan_form.title.data = book.title
 
-    form.title.data = book.title
-    if session_id:
-        if not reader:
-            flash("Usuário não encontrado.", "danger")
-            return redirect(url_for("main.new_loan", slug=book.slug))
-
-        if loan_service.has_active_loan(session_id):
-            flash(
-                f"{reader.fullname} já possui um empréstimo ativo.",
-                "warning",
-            )
-            session.pop("reader_id")
-            return redirect(url_for("main.new_loan", slug=book.slug))
-
-    if form.validate_on_submit():
-        if not session_id or not reader:
-            flash("Selecione um usuário para realizar o empréstimo.", "warning")
-            return redirect(url_for("main.new_loan", slug=book.slug))
-
+    if loan_form.validate_on_submit():
         try:
-            loan_service.create_loan(form, session_id, book.id)
-            book_service.borrow_book(book.id)
+            loan_service.create_loan(loan_form, reader, book)
 
             db.session.commit()
             flash("Empréstimo realizado!", "success")
         except ValueError as e:
             db.session.rollback()
             flash(f"Erro ao criar empréstimo: {str(e)}", "danger")
-        except Exception:
-            db.session.rollback()
-            flash(f"{reader.fullname} já alugou um livro. ", "danger")
 
         session.pop("reader_id")
         return redirect(url_for("main.index"))
 
     return render_template(
         "pages/new_loan.html",
-        form=form,
+        search_form=search_form,
+        loan_form=loan_form,
         book=book,
         reader=reader,
-        search_form=search_form,
         title="Novo Empréstimo",
     )
 
@@ -98,14 +78,21 @@ def return_book(slug):
         flash("Erro ao processar devolução!", "danger")
     return redirect(url_for("main.index"))
 
+
 # TODO ajustar a tora renew loan com tratamento de exceções e rollback
 @main.route("/emprestimos/renovar/<int:id>/", methods=["GET", "POST"])
 @login_required
 def renew_loan(id):
-    loan = db.session.query(BookLoan).filter_by(id=id).first()
+    search_form = SearchForm()
+    reader_id = session.get("reader_id")
+
+    loan = db.session.get(BookLoan, id)
+    reader = reader_service.get_valid_reader(reader_id)
+
+    search_form.search.data = reader.fullname
 
     if loan:
-        loan_service.create_loan(loan, loan.reader_id, loan.book_id)
+        loan_service.create_loan(loan, reader, loan.book_id)
         db.session.commit()
         flash("Empréstimo renovado com sucesso!", "success")
     else:
